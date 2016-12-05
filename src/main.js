f3b34ff8484362cb4206ae7d9521b19a01caf6c7
lxchuan12 <lxchuan12@163.com>
2016-12-05 23:52:33 +0800

    Vue构建单页应用

--- src/main.js
+++ src/main.js
@@ -1,5 +1,40 @@
+// src/main.js
 import Vue from 'vue'
 import App from './App'
+import Hello from './components/Hello.vue'
+
+import VueRouter from 'vue-router'
+import VueResource from 'vue-resource'
+
+import TimeEntries from './components/TimeEntries.vue'
+import TimeEntries from './components/Home.vue'
+
+// 注册两个组件
+
+Vue.use(VueResource)
+Vue.use(VueRouter)
+
+// 路由map
+router.map({
+	'/Home':{
+		component: Home
+	},
+	'/time-entries': {
+    component: TimeEntries,
+    subRoutes: {
+      '/log-time': {
+        component: LogTime
+      }
+    }
+  }
+})
+
+router.redirect({
+	'*':'/Home'
+})
+
+router.start(App,'#app')
+
 
 /* eslint-disable no-new */
 new Vue({
